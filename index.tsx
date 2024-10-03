c8af80e90e1f1bf6ed59665684a8b778edf47240
Francisco Leal <francisco@talentprotocol.com>
2024-10-03 13:53:13 +0100

    add database initialization and example base model

--- index.tsx
+++ index.tsx
@@ -2,13 +2,34 @@ import { config } from "./config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { Top } from "./components/top";
+import sequelize from "./database";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello from hono!"));
 app.get("/react", (c) => c.html(<Top messages={["Hello", "World"]} />));
 
-const start = () => {
+const connectDB = async () => {
+  let retries = 5;
+  await new Promise((res) => setTimeout(res, 3000)); // wait 3 seconds before trying to connect
+  while (retries) {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+      await sequelize.sync();
+      console.log("Models have been synchronized.");
+      break;
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      await new Promise((res) => setTimeout(res, 5000)); // wait 5 seconds
+    }
+  }
+};
+
+const start = async () => {
+  await connectDB();
   serve(
     {
       fetch: app.fetch,
