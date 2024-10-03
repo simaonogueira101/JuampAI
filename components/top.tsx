b54bb122335376410a8cc5affdeb466ced93cb54
Francisco Leal <francisco@talentprotocol.com>
2024-10-03 16:00:15 +0100

    add weaviate config

--- components/top.tsx
+++ components/top.tsx
@@ -1,12 +1,15 @@
 import type { FC } from "hono/jsx";
+
 import { Layout } from "./layout";
+import { useState } from "hono/jsx";
 
 export const Top: FC<{ messages: string[] }> = (props: {
   messages: string[];
 }) => {
+  const [content, setContent] = useState("teste");
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">Hello Hono!</h1>
+      <h1 className="text-3xl font-bold underline">Hello Hono! {content}</h1>
       <ul>
         {props.messages.map((message) => {
           return <li>{message}!!</li>;
