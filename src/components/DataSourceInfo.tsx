29cecd1ae1b98d7ca44687009ff20a8d4259a411
Simão Nogueira <simaonogueira@Simaos-MacBook-Pro.local>
2024-10-04 12:57:24 +0100

    Adds logo.

--- src/components/DataSourceInfo.tsx
+++ src/components/DataSourceInfo.tsx
@@ -5,8 +5,7 @@ export const DataSourceInfo: FC = () => {
     <div className="bg-gray-800 p-4 w-full mx-auto flex flex-col flex-row">
       <div className="text-white font-bold mb-4">Connection</div>
       <div className="bg-gray-600 p-3 flex items-center space-x-4">
-        {/* Placeholder for logo */}
-        <div className="w-12 h-12 bg-gray-400 flex-shrink-0"></div>
+        <img class="object-cover w-12 h-12 bg-gray-400 flex-shrink-0" src="https://docs.talentprotocol.com/~gitbook/image?url=https%3A%2F%2F716591960-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FQBODgw4E0VYy54eAMLgS%252Ficon%252FzU6t1ObBzys2seWOz9Py%252Falx53n8W_400x400.jpg%3Falt%3Dmedia%26token%3Dee6603a6-366e-4ad1-9c1c-f2d6ec783e5d&width=32&dpr=2&quality=100&sign=36ffb549&sv=1" />
         <div>
           <p className="text-white font-semibold">Talent Protocol Docs</p>
           <a
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,15 @@ export const Navbar: FC = () => {
           DevRel Assistant
         </a>
       </div>
-      <div className="flex space-x-4">
+      {/* TODO: Enable if necessary. */}
+      {/* <div className="flex space-x-4">
         <a href="/" data-navigo className="text-gray-300 hover:text-white hover:underline">
           Assistant
         </a>
         <a href="/config" data-navigo className="text-gray-300 hover:text-white hover:underline">
           Config
         </a>
-      </div>
+      </div> */}
     </nav>
   );
 };
